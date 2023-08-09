9465a9713ca74a8a30e3888f26bab6f5b195ed63
Paul Matthew <116613750+paul-matthew@users.noreply.github.com>
2023-08-09 00:06:40 -0400

    Baseline 1.5
    Update to include local strapi APItoken for the profuction APItoken as a placeholder.

--- app/api/get-data-from-strapi.server.js
+++ app/api/get-data-from-strapi.server.js
@@ -9,8 +9,8 @@ try {
     apiToken = "2d1cc176200bc504886bd120dbbb5b862bfa9d7b94581bedab26a1acae5650970bf6185d55a4cc9adda13b97d1706b5b60d4b15d8fbad32bdbca2ddc5d05e3afbb1c1783031b7ad14af7bd49541119c3842c93f7a37b7c0857fd11dd42067b1ddbd768315fd17d407fc631ba6766fbb9494b7c6634d597ed91eaacc3806b2474";
     console.log("This is a local build yo");
   } else {
-    baseUrl = "https://journeyswkris-938066b97596.herokuapp.com/"; //UPDATE THIS SECTION
-    apiToken = "e24148405165ce1ea76caf74504d058ac49892d30d3a6b8c64af9bed17471354619a0a87886c0e93d619740fce277dceb9e256c84bd35ce3c1d50aba294b150bc0916c0efb2bfce586dfc668e290cff2a0cca4602029a779ed080d27c536d7d2b87a4098981e39d5e909a7296ea4ac995b0976a64160cd6da51b40c0d7a16cec";
+    baseUrl = "https://journeyswkris-938066b97596.herokuapp.com/"; //UPDATE SECTION API TOKEN
+    apiToken = "2d1cc176200bc504886bd120dbbb5b862bfa9d7b94581bedab26a1acae5650970bf6185d55a4cc9adda13b97d1706b5b60d4b15d8fbad32bdbca2ddc5d05e3afbb1c1783031b7ad14af7bd49541119c3842c93f7a37b7c0857fd11dd42067b1ddbd768315fd17d407fc631ba6766fbb9494b7c6634d597ed91eaacc3806b2474";
     console.log("This is a production build");
   }
   const url = `${baseUrl}/api/${path}?${query}`;
